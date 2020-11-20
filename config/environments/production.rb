266d97d119b39354502199c0a34539653ada41d0
Dmitry <antonenkodima12@gmail.com>
2020-11-20 18:29:22 +0300

    update prod configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,12 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.assets.compile = true
+
+  config.serve_static_assets = true
+  config.require_master_key = false
+  config.secret_key_base = 'blipblapblup'
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
