70f50529682ea8dc9e570cfe7bbf9fc46b744d75
Dmitry <antonenkodima12@gmail.com>
2020-11-21 18:36:07 +0300

    fix changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,13 @@
 class Question < ApplicationRecord
+  # scope :active, -> {
+  #   where.not(answer_wrong_1_ru: true,
+  #             answer_wrong_2_ru: true,
+  #             answer_wrong_3_ru: true).order('id DESC')
+  # }
+
   scope :active, -> {
-    where(deteled: false).order('id DESC')
-  }
+    where.not("answer_wrong_1_ru IS NOT NULL AND answer_wrong_2_ru IS NOT NULL AND answer_wrong_3_ru IS NOT NULL").order('id DESC')
+    }
 
 
   scope :deleted, -> { where(deteled: true).order('id DESC') }
