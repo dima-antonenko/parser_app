66c0934a99a77eb0618cfa2c5e5172218a7b6b1a
Dmitry <antonenkodima12@gmail.com>
2020-11-21 17:18:45 +0300

    fix changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,17 @@
 class Question < ApplicationRecord
-  scope :active, -> { where(deteled: false).order('id DESC') }
+  scope :active, -> {
+    where(deteled: false).order('id DESC')
+  }
+
+
   scope :deleted, -> { where(deteled: true).order('id DESC') }
-  scope :clear, -> { where.not(title: nil).where.not(answer: nil).where.not(answer_wrong_1_ru: nil).where.not(answer_wrong_2_ru: nil).where.not(answer_wrong_3_ru: nil).order('id DESC') }
+
+  scope :clear, -> { where.not(title: nil)
+                       .where.not(answer: nil)
+                       .where.not(answer_wrong_1_ru: nil)
+                       .where.not(answer_wrong_2_ru: nil)
+                       .where.not(answer_wrong_3_ru: nil)
+                       .where(deteled: false).order('id DESC') }
 
 
   validates :title, :answer, :answer_wrong_1_ru, :answer_wrong_2_ru, :answer_wrong_3_ru, presence: true
